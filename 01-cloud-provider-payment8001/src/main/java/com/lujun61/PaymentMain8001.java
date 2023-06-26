73f400da8180b456a3769650ceb8ddc581e887d8
陆俊 <lujun61@outlook.com>
2023-06-26 14:13:16 +0800

    分布式微服务-初级操作
    单点Eureka服务与Demo服务的注册与发现

--- 01-cloud-provider-payment8001/src/main/java/com/lujun61/PaymentMain8001.java
+++ 01-cloud-provider-payment8001/src/main/java/com/lujun61/PaymentMain8001.java
@@ -3,9 +3,11 @@ package com.lujun61;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.lujun61.dao")
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
